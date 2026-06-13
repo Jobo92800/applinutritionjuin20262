357ef9d51c10b52092d078073fb3b25ccb52ec12
Jobo92800 <158471772+Jobo92800@users.noreply.github.com>
2026-06-13 15:32:44 +0200

    Updated package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import ShoppingList from './components/ShoppingList';
 import ProgressTracking from './components/ProgressTracking';
 import AdminPanel from './components/AdminPanel';
 import AccountProfile from './components/AccountProfile';
-import FoodAnalyzer from './components/FoodAnalyzer';
 
 function AppContent() {
   const { user, isLoading, completeOnboarding } = useAuth();
@@ -110,8 +109,6 @@ function AppContent() {
         return <ShoppingList />;
       case 'progress':
         return <ProgressTracking />;
-      case 'food-analyzer':
-        return <FoodAnalyzer />;
       case 'account':
         return <AccountProfile />;
       case 'admin':
